ddf273bb737bf0cfa95b0b56452835a13a73c923
anujneupane <anujneupane987@gmail.com>
2025-03-26 15:03:09 +0545

    go

--- account/utils.py
+++ account/utils.py
@@ -3,11 +3,11 @@ import os
 
 class Util:
     @staticmethod
-    def send_mail(data):
+    def send_email(data):
         email = EmailMessage(
             subject=data['subject'],
             body =  data['body'],
             from_email= os.environ.get('EMAIL_FROM'),
             to=[data['to_email']]
         )
-        email.save()
+        email.send()
